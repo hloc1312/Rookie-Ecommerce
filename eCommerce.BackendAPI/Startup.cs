1a07b0425b2888e26b898610367b8ce18248a4ad
hloc1312 <hloc878@gmail.com>
2022-03-04 23:06:21 +0700

    config startup

--- eCommerce.BackendAPI/Startup.cs
+++ eCommerce.BackendAPI/Startup.cs
@@ -7,6 +7,7 @@ using eCommerce.Utilities.Constants;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -14,7 +15,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace eCommerce.BackendAPI
 {
